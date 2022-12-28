1608824eae7e2f13f1092af6b37d53307ea74b03
Rosinery <92558971+Rosinery@users.noreply.github.com>
2022-12-28 17:12:35 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA8O3rAloqIwhwzeFkDnscWf4tlitWDteE",
+  authDomain: "narracao1.firebaseapp.com",
+  projectId: "narracao1",
+  storageBucket: "narracao1.appspot.com",
+  messagingSenderId: "531290063897",
+  appId: "1:531290063897:web:156cf4646bd5a360e51688"
+}
